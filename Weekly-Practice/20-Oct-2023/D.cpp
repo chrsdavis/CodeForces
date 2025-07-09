1579160797ec90eacc3587da363b76f609366737
chrsdavis <84039785+chrsdavis@users.noreply.github.com>
2025-07-08 21:34:32 -0700

    practice

---
+++ Weekly-Practice/20-Oct-2023/D.cpp
@@ -0,0 +1,16 @@
+#include <vector>
+#include <iostream>
+#include <iomanip>
+#include <ios>
+
+using namespace std;
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int num_a, num_b, num_t;
+    cin >> num_a >> num_b >> num_t;
+
+    vector<int>
+}
